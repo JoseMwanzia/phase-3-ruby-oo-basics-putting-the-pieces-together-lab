f82218ba7a3d1a43d2f4943b0611122c985dfca2
joseph mwanzia <makamwanzia@gmail.com>
2023-02-12 20:05:08 +0300

    Completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require 'pry'
 
+class Book
+    attr_reader :title, :turn_page
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+        @title=title
+    end
+
+    def turn_page  
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("And Then There Were None")
+
+# binding.pry
+# book.title =
 
